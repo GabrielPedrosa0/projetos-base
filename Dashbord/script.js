51691b9bae9c68f93a0ccb7fc106ca641a9365d8
Gabriel Pedrosa <gabrielpedrosa346@gmail.com>
2026-01-25 08:36:58 -0300

    botão de remover paciente e usg do paciente criado; Adição de escolha das usgs que o paciente fez por meio de select

--- Dashbord/script.js
+++ Dashbord/script.js
@@ -6,6 +6,7 @@ function addPaciente(){
     const nome = nomePaciente.value
     const cns = cnsPaciente.value
 
+
     if(nome.trim() === "" || cns.trim() === ""){
         alert("Preencha os campos!!")
         return
@@ -14,16 +15,56 @@ function addPaciente(){
     const paciente = document.createElement('div')
     paciente.className = "paciente"
 
+    const btnRemove = document.createElement('button')
+    btnRemove.className = 'btnRemove'
+    btnRemove.textContent = 'Remover'
+
+    btnRemove.addEventListener('click', () => {
+        paciente.remove()
+    })
+
     const pNome = document.createElement('p')
     pNome.textContent = `Nome: ${nomePaciente.value}`
 
     const pCns = document.createElement('p')
     pCns.textContent = `CNS: ${cnsPaciente.value}`
 
+
     paciente.appendChild(pNome)
     paciente.appendChild(pCns)
+    paciente.appendChild(btnRemove)
     exames_ativos.appendChild(paciente)
 
     nomePaciente.value = ''
     cnsPaciente.value = ''
+      
 }
+
+
+
+function addUSG(){
+    const pacienteUSG = document.getElementById('usg')
+    const listUsgs = document.querySelector('.list_usgs')
+    
+    const pUSG = pacienteUSG.value
+
+    const usgRemove = document.createElement('button')
+    usgRemove.className = 'usgRemove'
+    usgRemove.textContent = 'X'
+
+    usgRemove.addEventListener('click', () => {
+        list.remove()
+    })
+
+    const list = document.createElement('li')
+    list.textContent = `${pUSG}`
+    list.className = 'pUSGS'
+
+    listUsgs.appendChild(list)
+    list.appendChild(usgRemove)
+
+    pacienteUSG.value = ''   
+
+    
+    
+} 
