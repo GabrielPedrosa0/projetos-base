08e4407a41e86f2983a47f27451b292669a32ecb
Gabriel Pedrosa <gabrielpedrosa346@gmail.com>
2026-02-01 09:28:29 -0300

    aviso ao user de prencher todos os campos, pequenas estilizaçoes

--- Dashbord/script.js
+++ Dashbord/script.js
@@ -2,13 +2,25 @@ function addPaciente(){
     const exames_ativos = document.querySelector('.exames-ativos')
     const nomePaciente = document.getElementById('nome_paciente')
     const cnsPaciente = document.getElementById('cns_paciente')
+    const dataExame = document.getElementById('exame_date')
+    const aviso = document.querySelector('.aviso')
 
     const nome = nomePaciente.value
     const cns = cnsPaciente.value
+    const data = dataExame.value
 
 
-    if(nome.trim() === "" || cns.trim() === ""){
-        alert("Preencha os campos!!")
+    if(nome.trim() === "" || cns.trim() === "" || data.trim() === ""){
+        aviso.classList.add('show')
+        nomePaciente.classList.add('importatante')
+        cnsPaciente.classList.add('importatante')
+
+        setTimeout(function(){
+            aviso.classList.remove('show')
+            nomePaciente.classList.remove('importatante')
+            cnsPaciente.classList.remove('importatante')
+        }, 3000)
+        
         return
     }
 
@@ -17,7 +29,7 @@ function addPaciente(){
 
     const btnRemove = document.createElement('button')
     btnRemove.className = 'btnRemove'
-    btnRemove.textContent = 'Remover'
+    btnRemove.innerHTML = '<i class="ri-user-minus-fill"></i> Remover paciente'
 
     btnRemove.addEventListener('click', () => {
         paciente.remove()
@@ -29,14 +41,19 @@ function addPaciente(){
     const pCns = document.createElement('p')
     pCns.textContent = `CNS: ${cnsPaciente.value}`
 
+    const pdate = document.createElement('p')
+    pdate.textContent = `Data do exame: ${data}`
+
 
     paciente.appendChild(pNome)
     paciente.appendChild(pCns)
+    paciente.appendChild(pdate)
     paciente.appendChild(btnRemove)
     exames_ativos.appendChild(paciente)
 
     nomePaciente.value = ''
     cnsPaciente.value = ''
+    dataExame.value = ''
       
 }
 
@@ -50,7 +67,7 @@ function addUSG(){
 
     const usgRemove = document.createElement('button')
     usgRemove.className = 'usgRemove'
-    usgRemove.textContent = 'X'
+    usgRemove.innerHTML = '<i class="ri-close-circle-fill"></i>'
 
     usgRemove.addEventListener('click', () => {
         list.remove()
